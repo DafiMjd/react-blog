195bee96d1f8c10f999bbbc8bbaded440bbf16ae
DafiMjd <dafi.majid@gmail.com>
2023-01-04 22:57:50 +0700

    custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,22 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
   // const handleClick = (e) => {
   //   console.log("Hello world", e);
   // };
-
   // const handleClickAgain = (name, e) => {
   //   console.log("hello " + name, e.target);
   // };
 
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-
-  const [name, setName] = useState('dafi');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-
-    setBlogs(newBlogs);
-  };
-
-  useEffect(() => {
-    console.log('use effect ran');
-  }, [name]);
+  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-      <button onClick={() => setName('Majid')}>change name</button>
-      <p>{name}</p>
+      {error && <div> {error} </div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
