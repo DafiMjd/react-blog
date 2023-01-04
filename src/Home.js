93ec128859368d800d320e57797553290455a1e8
Dafi Majid F <dafi.majid@megafinance.co.id>
2023-01-04 15:26:16 +0700

    hook useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,45 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return (  
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
-    );
-}
- 
+  // const handleClick = (e) => {
+  //   console.log("Hello world", e);
+  // };
+
+  // const handleClickAgain = (name, e) => {
+  //   console.log("hello " + name, e.target);
+  // };
+
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+
+  const [name, setName] = useState('dafi');
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log('use effect ran');
+  }, [name]);
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+      <button onClick={() => setName('Majid')}>change name</button>
+      <p>{name}</p>
+    </div>
+  );
+};
+
 export default Home;
